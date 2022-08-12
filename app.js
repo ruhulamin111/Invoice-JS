08a643037c03079746362e40f618668931ad8779
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-12 07:13:45 +0600

    product info

--- app.js
+++ app.js
@@ -4,5 +4,23 @@ const buyerDetails = document.getElementById('detail-submit-btn').addEventListen
     const buyerOutput = document.getElementById('buyer-info');
     buyerOutput.innerText = buyerInput;
     buyerInput.value = '';
+})
+
+const productDetails = document.getElementById('add-details-btn').addEventListener('click', function (event) {
+    const productName = document.getElementById('item-name-input').value;
+    const productPrice = document.getElementById('item-price-input').value;
+    const productQunatity = document.getElementById('item-quantity-input').value;
+    const productTotal = productPrice * productQunatity;
+
+    const productOutputContainer = document.getElementById('info-table');
+    const productOutputTr = document.createElement('tr');
+    productOutputTr.innerHTML = `
+    <td>${productName}</td>
+    <td>${productPrice}</td>
+    <td>${productQunatity}</td>
+    <td>${productTotal}</td>
+    `;
+    productOutputContainer.appendChild(productOutputTr);
+
 
 })
